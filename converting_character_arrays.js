3c96c3f53b29d62ea7eac2d234a7000acadf176f
benji_707 <dontjunkthis@gmail.com>
2025-07-27 22:02:33 -0500

    added character arrays

---
+++ converting_character_arrays.js
@@ -0,0 +1,59 @@
+// Secret Agency Message Decoder System
+
+// Task 1: Decode the Following Reversed Messages
+// Message 1
+let encryptedMessage1 = "!yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+console.log('Encrypted Message 1:', encryptedMessage1);
+let decryptedMessage1 = encryptedMessage1.split('').reverse().join('');
+console.log('Decrypted Message 1:', decryptedMessage1);
+console.log('');
+
+// Message 2
+let encryptedMessage2 = "!ecitcarp htiw retteb teg ll'uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+console.log('Encrypted Message 2:', encryptedMessage2);
+let decryptedMessage2 = encryptedMessage2.split('').reverse().join('');
+console.log('Decrypted Message 2:', decryptedMessage2);
+console.log('');
+
+// Message 3
+let encryptedMessage3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+console.log('Encrypted Message 3:', encryptedMessage3);
+let decryptedMessage3 = encryptedMessage3.split('').reverse().join('');
+console.log('Decrypted Message 3:', decryptedMessage3);
+console.log('');
+
+// Message 4
+let encryptedMessage4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+console.log('Encrypted Message 4:', encryptedMessage4);
+let decryptedMessage4 = encryptedMessage4.split('').reverse().join('');
+console.log('Decrypted Message 4:', decryptedMessage4);
+console.log('');
+
+// Task 2: Write Your Own Reverse Messages
+// Original Message 1
+let originalMessage1 = "You are stronger than you think!";
+console.log('Original Message 1:', originalMessage1);
+let encryptedOwn1 = originalMessage1.split('').reverse().join('');
+console.log('Encrypted Message 1:', encryptedOwn1);
+console.log('');
+
+// Original Message 2
+let originalMessage2 = "Practice makes perfect in coding.";
+console.log('Original Message 2:', originalMessage2);
+let encryptedOwn2 = originalMessage2.split('').reverse().join('');
+console.log('Encrypted Message 2:', encryptedOwn2);
+console.log('');
+
+// Original Message 3
+let originalMessage3 = "Every expert was once a beginner.";
+console.log('Original Message 3:', originalMessage3);
+let encryptedOwn3 = originalMessage3.split('').reverse().join('');
+console.log('Encrypted Message 3:', encryptedOwn3);
+console.log('');
+
+// Original Message 4
+let originalMessage4 = "Believe in yourself and keep going!";
+console.log('Original Message 4:', originalMessage4);
+let encryptedOwn4 = originalMessage4.split('').reverse().join('');
+console.log('Encrypted Message 4:', encryptedOwn4);
+console.log('');
